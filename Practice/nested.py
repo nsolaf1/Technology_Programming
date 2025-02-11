233fcdd8e113b84f6850098cccbef1e1a4f16f4a
NSOLAf1 <nsolaf1@gmail.com>
2025-02-11 15:22:30 +0600

    class

--- Practice/nested.py
+++ Practice/nested.py
@@ -1,11 +1,53 @@
-students = [
-    ["John", "Doe", 21, "Physics"],
-    ["Jane", "Smith", 22, "Chemistry"],
-    ["Mark", "Johnson", 20, "Mathematics"],
-    ["Emily", "Davis", 23, "Biology"]
-]
+# students = [
+#     ["John", "Doe", 21, "Physics"],
+#     ["Jane", "Smith", 22, "Chemistry"],
+#     ["Mark", "Johnson", 20, "Mathematics"],
+#     ["Emily", "Davis", 23, "Biology"]
+# ]
 
-for x in students:
-    # print(x[1])
-    for y in x:
-        print(y)
+# for x in students:
+#     # print(x[1])
+#     for y in x:
+#         print(y)
+
+
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   print(x)
+
+# for x in "banana":
+#   print(x )
+
+# sum = 0
+# for x in range(2, 51, 2):
+#     sum += x
+
+# print(sum )
+
+#Multiplication Table 5x1 = 5, 5x2 = 10
+
+# x = 5
+# y = 1
+
+# for x in range(1, 11):
+#     for y in range (1, 11):
+#         print(f"{x} x {y} = {x*y}")
+#     print()
+
+
+
+# x = 9
+# for i in range (1, 11):
+#     print(f"{x} x {i} = {x*i}")
+
+
+
+
+for i in range(1, 11):
+    for j in range(1, i + 1):
+        print(j, end="" )
+    print()
+
+
+
--- Practice/while.py
+++ Practice/while.py
@@ -2,4 +2,4 @@ password  = input("Enter pass: ")
 x = "python1234"
 
 while True:
-    
+    print("Hello")
