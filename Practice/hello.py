c4d38e7d952e2c09b98135921de966e1c815655c
NSOLAf1 <nsolaf1@gmail.com>
2026-01-26 13:36:04 +0600

    l1

---
+++ Practice/hello.py
@@ -0,0 +1,8 @@
+age = int(input("Enter age: "))
+name = input("Enter your name: ")
+
+if age >= 18:
+    print(f"Hello {name}, you are an adult, {age}.")
+else:
+    print(f"Hello {name}, you are a minor. {age}")
+    
---
+++ Practice/loop.py
@@ -0,0 +1,34 @@
+#For loop (1..5)
+# print("For Loop")
+# for i in range(1, 6):
+#     print(i)
+
+#while( 1..55)
+# print("While Loop")
+# count = 1
+# while count <= 5:
+#     print(count)
+#     count +=1
+    
+
+# 1..55
+# odd *= 3
+# even = "EVEN"
+# sum=()
+
+# for i in range(1,56):
+#     if i % 2 != 0:
+#         print(i, "-> ", i * 3, "odd " )
+#     else:
+#         print(i, "-> " , i, "even ")
+        
+# while( 1,55)
+print("While Loop")
+count = 1
+while count <= 5:
+    if count % 2 != 0:
+        print("Odd")
+    else:
+        print("Event")
+        
+    count +=1
